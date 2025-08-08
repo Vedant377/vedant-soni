09d12619cefb30d74378e88d679fac2015af76ea
Vedant377 <vedantjsoni99@gmail.com>
2025-08-08 13:00:39 +0530

    Add files via upload

---
+++ vedant soni/third.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the number a : ");
+scanf("%d",&a);
+
+printf("enter the number b : ");
+scanf("%d",&b);
+
+
+int substract = a-b;
+printf("substract of a and b is %d",substract);
+
+return 0;
+}
