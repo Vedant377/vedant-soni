9243571534e25b0fd361e4c134491549da3e1989
Vedant377 <vedantjsoni99@gmail.com>
2025-08-08 13:03:02 +0530

    Add files via upload

---
+++ vedant soni/vedant soni/fifth.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+float main()
+{
+float d,b;
+int a;
+printf("enter the number a : ");
+scanf("%d",&a);
+printf("enter the number b : ");
+scanf("%f",&b);
+d = a/b;
+printf("%d/%f=%f",a,b,d);
+
+return 0;
+}
---
+++ vedant soni/vedant soni/second.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the number a : ");
+scanf("%d",&a);
+
+printf("enter the number b : ");
+scanf("%d",&b);
+
+
+int sum = a+b;
+printf("sum of a and b is %d",sum);
+
+return 0;
+}
