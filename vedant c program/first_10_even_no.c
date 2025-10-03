38cd7add67eff27a1d81bef32c53500ca0392b7b
Vedant377 <vedantjsoni99@gmail.com>
2025-10-03 20:29:41 +0530

    Delete vedant c program directory

--- vedant c program/first_10_even_no.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-int main() {
-    printf("The first 10 even numbers are:\n");
-    for(int i=1;i<=10;i++){
-        printf("%d\n",2*i);
-    }
-    return 0;
-}
--- vedant c program/print_parallel-loop-part-A.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main (){
-    for (int i=5,j=1;i>=1,j<=5;i--,j++){
-        printf("%d %d\n",i,j);
-    }
-    return 0;
-}
