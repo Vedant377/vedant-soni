3ad0046d9ecbdf5f697675b317c8c7d04df55d20
Vedant377 <vedantjsoni99@gmail.com>
2025-10-10 11:31:55 +0530

    Update calculation_avg_three_sub_for_given_grade.c

--- vedant c program/calculation_avg_three_sub_for_given_grade.c
+++ vedant c program/calculation_avg_three_sub_for_given_grade.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 int main() {
-    int maths,physics,chemistry;
+    float maths,physics,chemistry;
     printf("Enter marks for Maths, Physics, and Chemistry:\n");
     scanf("%d\n%d\n%d", &maths, &physics, &chemistry);
     if (maths >= 0 && physics >= 0 && chemistry >= 0) {
         float average = (maths + physics + chemistry) / 3.0;
         float total = (maths + physics + chemistry);
         printf("Average of the three subjects: %f\n", average);
-        printf("Total of the three subjects : %d+%d+%d=%d\n", maths, physics, chemistry, total);
+        printf("Total of the three subjects : %f+%f+%f=%f\n", maths, physics, chemistry, total);
         if (average >= 70) {
             printf("Grade: distinction\n");
         } else if (average >= 60) {
@@ -26,6 +26,7 @@ int main() {
     }
     return 0;
 }
+
 
 
 
