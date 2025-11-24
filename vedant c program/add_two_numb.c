66488aa0e7ed4a8ff938c1babaa62bd714fed07c
Vedant377 <vedantjsoni99@gmail.com>
2025-11-25 00:48:29 +0530

    Delete vedant c program directory

--- vedant c program/add_two_numb.c
+++
@@ -1,9 +0,0 @@
-# include<stdio.h> 
-int main(){
-    int a,b;
-    printf("Enter any two numbers for addition\n");
-    scanf("%d %d",&a,&b);
-    int sum = a+b;
-    printf("%d+%d=%d",a,b,sum);
-    return 0;
-}
